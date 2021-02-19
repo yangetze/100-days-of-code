f3c115a6f985cbb7058d2ccf54f46e7e438c37d5
Yangetze González <yangetze@gmail.com>
2021-02-18 23:51:04 -0400

    Create randomWholeNumberWithinARange.js

---
+++ R1/randomWholeNumberWithinARange.js
@@ -0,0 +1,8 @@
+// Generate Random Whole Numbers within a Range
+
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(randomRange(2, 20));
+  
